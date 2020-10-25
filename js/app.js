2b9909a48bdfd548396b2446447336d936869f50
AbdulSamad Ayoade <ayoadesamad@gmail.com>
2020-10-25 13:16:54 +0100

    create app.js

---
+++ js/app.js
@@ -0,0 +1,12 @@
+// MOBILE NAVIGATION MENU
+
+const menuButton = document.querySelector('.menu_toggle');
+const open = document.querySelector('.open');
+const close = document.querySelector('.close');
+const navList = document.querySelector('.nav_list');
+
+menuButton.addEventListener('click', function () {
+    navList.classList.toggle('active');
+    open.classList.toggle('active');
+    close.classList.toggle('active');
+});
